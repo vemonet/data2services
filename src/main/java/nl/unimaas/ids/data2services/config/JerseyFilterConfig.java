afcad03224104c3100f6de3496e10521788cde4d
nunogit <nunomsilvanunes@gmail.com>
2018-11-21 16:09:35 +0100

    config file

---
+++ src/main/java/nl/unimaas/ids/data2services/config/JerseyFilterConfig.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.unimaas.ids.data2services.config;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JerseyFilterConfig implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request,
+            ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        
+        System.out.println("hello World filter");
+        response.getHeaders().add("Access-Control-Allow-Headers",
+              "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
