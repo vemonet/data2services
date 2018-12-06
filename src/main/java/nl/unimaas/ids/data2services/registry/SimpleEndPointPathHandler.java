39df3a3b4baefc60c13759578690c043d1ab9f6e
nunogit <nunomsilvanunes@gmail.com>
2018-12-06 19:06:15 +0100

    start working on #4 solution

---
+++ src/main/java/nl/unimaas/ids/data2services/registry/SimpleEndPointPathHandler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.unimaas.ids.data2services.registry;
+
+/**
+ *
+ * @author nuno
+ */
+public class SimpleEndPointPathHandler extends PathHandler{
+    public SimpleEndPointPathHandler(){
+        this.setDomain("test");
+        this.setPathHandlerModel("/{testx}/{test2}/{test3}");
+        setupSwaggerOperation();
+    }
+    
+    
+    @Override
+    public String process(String path) {
+        return "this is a test for "+path;
+    }
+    
+}
