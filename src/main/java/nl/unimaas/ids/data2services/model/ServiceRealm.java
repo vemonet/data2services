ca4183835c402672c131324bf2442f049a25fddb
nunogit <nunomsilvanunes@gmail.com>
2018-12-12 17:21:58 +0100

    improvement

---
+++ src/main/java/nl/unimaas/ids/data2services/model/ServiceRealm.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.unimaas.ids.data2services.model;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author nuno
+ */
+public class ServiceRealm {
+    String serviceRealm = null;
+
+    public ServiceRealm(String serviceDomain) {
+        this.serviceRealm = serviceDomain;
+    }
+
+    public ServiceRealm() {
+        serviceRealm = null;
+    }
+    
+    public Optional<String> getRealm(){
+        return Optional.ofNullable(serviceRealm);
+    }
+}
